e52d8db47568d47e5ee437a670a9c3b079d4d8c3
pavez845 <134526243+pavez845@users.noreply.github.com>
2024-04-23 22:59:43 -0400

    trabajo
    No nos dimos cuenta que era que empezar con el git

---
+++ js.js
@@ -0,0 +1,21 @@
+function generarCarta() {
+    var nombre = document.getElementById("nombre").value;
+    var apellidoPaterno = document.getElementById("apellidop").value;
+    var apellidoMaterno = document.getElementById("apellidom").value;
+    var rut = document.getElementById("rut").value;
+    var fecha = document.getElementById("fecha_nacimiento").value;
+    var edad = document.getElementById("edad").value;
+    var celular = document.getElementById("celular").value;
+    var email = document.getElementById("email").value;
+    var profesion = document.getElementById("profesion").value;
+    var genero = document.getElementById("genero").value;
+    var motivacion = document.getElementById("motivacion").value;
+
+    const cartaText = "Estimad@ me presento como " + nombre + " " + apellidoPaterno + " " + apellidoMaterno +
+        ", RUT " + rut + ", de " + edad + " años, soy del género " + genero +
+        ", mi fecha de nacimiento es " + fecha + ", actualmente tengo la profesión de " +
+        profesion + ", mi motivación para entrar aquí es: " + motivacion +
+        ", mi número es " + celular + ". Si desea contactarme, le dejo mi email: " + email;
+        
+    document.getElementById("carta").value = cartaText;
+}
